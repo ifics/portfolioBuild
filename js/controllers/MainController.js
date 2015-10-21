19661af204598fb6f2c366dbebe186c5a9d17988
ifics <blakejohnson.pdx@gmail.com>
2015-10-21 13:56:59 -0700

    issue with file linking. Troubleshooting starts here

---
+++ js/controllers/MainController.js
@@ -0,0 +1,19 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.lifestyle = [ 
+	   { 
+	    icon: 'img/sloth-life.jpg', 
+	    title: 'MOVE'
+	    
+	  },
+	  {
+	  	icon: 'img/social-medial.jpg'
+	  }
+
+	$scope.clickTest = function(){
+ 		alert("hello World");
+	];
+
+ 
+ 
+
+}]);
---
+++ js/directives/lifestyle.js
@@ -0,0 +1,11 @@
+app.directive('lifestyle', function() {
+  return {
+    restrict: 'E', 
+    scope: { 
+      info: '=' 
+    }, 
+    templateUrl: 'js/directives/lifestyle.html' 
+  }; 
+})
+    
+})  
